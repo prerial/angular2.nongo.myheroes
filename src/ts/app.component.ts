42e6c8dc6ca35ade6da2f06b9c48f0bb9f90e13e
Mikhail Grinshteyn <mgrinsht@gmail.com>
2017-02-11 18:59:12 -0500

    clean

--- src/ts/app.component.ts
+++ src/ts/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   moduleId: module.id,
@@ -7,6 +7,6 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: [ '../css/app.component.css' ]
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Tour of Heroes';
 }
